d1ff48a61941f1c25de8ea74186d7218f13cf974
Siddharth-codess <siddharthjoshi.24011564@gehu.ac.in>
2026-02-05 13:01:55 +0530

    Week-2, Q1

--- q1.cpp
+++ q1.cpp
@@ -1,37 +1,75 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    int T;
-    cin>>T;
-    while(T--){
-        int n;
-        cin>>n; 
-        int arr[1000];
-        for(int i =0 ; i<n ; i++){
-            cin>>arr[i];
+// first occurence of key
+int firstoccurence (int arr[],int n ,int key){
+    int low = 0, high = n-1;
+    int result =-1;
+
+    while(low<=high){
+        int mid=(low + (high-low))/2;
+
+        if(arr[mid]==key){
+            result = mid;
+            high = mid-1;
+        }
+        else if(arr[mid]<key){
+            low = mid +1;
+        }
+        else {
+            high = mid -1;
+        }
+    
+    
+}
+return result;
+}
+
+//second occurence of key
+int lastoccurence(int arr[] , int n, int key){
+    int low = 0;
+    int high =n-1;
+    int result =-1;
+    while(low<=high){
+        int mid =(low + (high-low))/2;
+
+        if(arr[mid]==key){
+            result=mid;
+            low=mid+1;
         }
 
-        int key;
-        cin>>key; 
-        
-        int comparisons =0;
-        bool found =false;
-
-        for (int i = 0;i<n;i++){
-            comparisons++;
-            if(key==arr[i]){
-                found = true;
-                break;
-            }
-        }
-        if(found){
-            cout<<"number is found and comparisons are: "<<comparisons;
+        else if(arr[mid]<key){
+            low = mid+1;
         }
         else{
-            cout<<"number is not found " <<comparisons;
+            high=mid -1;
         }
     }
+    return result ;
+}
+int main(){
+    int n;
+    cin>> n ;
+    int arr[n];
+
+    for(int i =0; i <n;i++){
+        cin>>arr[i];
+    }
+
+    int key;
+    cin>>key;
+
+    int first = firstoccurence(arr, n , key);
+    if(first == -1){
+        cout<<"key not present";
+    } 
+    else{
+        int last = lastoccurence(arr,n,key);
+        int count =last - first +1;
+        cout<<"total number of ourrence are : "<<count ;
+    }
+
+
 
     return 0;
 }
